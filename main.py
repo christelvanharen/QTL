40e192c439f28d3e557a93e172ff4fe32625e7f3
jaspe <Jasper_versantvoort@hotmail.com>
2021-05-27 10:57:07 +0200

    Commit

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from scipy.stats import chisquare
 def markers_finder(file):
     i = 1
     genes = []  # gen1, gen2
-    markers = []  # aaabbbaaa, aa
+    markers = []  # aaabbbaaa, aabbbaaa
     for regel in file:
         if ("; " + str(i)) in regel:
             gen = regel.split(" ")[0]
@@ -26,11 +26,31 @@ def chisq_test(genes, markers):
 
 
 def compair_dif(genes, markers):
-    differences = []
+    rf_list = []
     for i in range(len(markers)):
         for j in range(i + 1, len(markers)):
-            print("dif: ", genes[i], genes[j])
-            calculate_dif(markers[i], markers[j])
+            # print("dif: ", genes[i], genes[j])
+            sub_list = [genes[i], genes[j]]
+            sub_list.append(calculate_dif(markers[i], markers[j]))
+            rf_list.append(sub_list)
+    # print(rf_list)
+    order_genes(rf_list)
+
+
+def order_genes(rf_list):
+    order_list = []
+    higest_rf = 0
+    for sub_list in rf_list:
+        if sub_list[2] > higest_rf:
+            higest_rf = sub_list[2]
+            gen_name_one = sub_list[0]
+            gen_name_two = sub_list[1]
+
+    print(higest_rf, gen_name_one, gen_name_two)
+    order_list.append([gen_name_one, 0])
+    for sub_list in rf_list:
+        if sub_list[0] == gen_name_one or sub_list[1] == gen_name_one:
+            print(sub_list)
 
 
 def calculate_dif(marker_a, marker_b):
@@ -42,10 +62,14 @@ def calculate_dif(marker_a, marker_b):
         else:
             if marker_a[i] != marker_b[i]:
                 difference += 1
-    print(difference)
-    rf = difference/(len(marker_a)-stripes)*100
-    print(rf)
-    print(stripes)
+
+    rf = difference / (len(marker_a) - stripes) * 100
+
+    return rf
+
+
+# def make_file():
+#     file = open("MapChart_file.txt", "w+")
 
 
 def main():
